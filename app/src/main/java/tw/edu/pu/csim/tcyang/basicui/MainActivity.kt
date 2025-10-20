0dcd5b244e0c12bc5b3149320e9e0bb85861cf8b
xuelin123yt <xuelin123yt@gmail.com>
2025-10-20 15:58:37 +0800

    V4 三顆按鈕設計及結束系統

--- app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
@@ -8,19 +8,28 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.CheckboxDefaults.colors
+import androidx.compose.material3.FloatingActionButtonDefaults.elevation
+import androidx.compose.material3.FloatingActionButtonDefaults.shape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -151,25 +160,49 @@ fun Main(modifier: Modifier = Modifier) {
         ) {
             Button(
                 onClick = {
-                    // Toast.makeText(context, "歡迎修課", Toast.LENGTH_SHORT).show()
-                }
+                    mper?.release()  //釋放資源
+                    mper = null // 清除舊引用
+                    mper = MediaPlayer.create(context, R.raw.tcyang) //設定音樂
+                    mper?.start()  } , //開始播放
+                modifier = Modifier
+                    .fillMaxWidth(0.33f)
+                    .fillMaxHeight(0.8f),
+                colors = buttonColors(Color.Green)
             ) {
-                Text(text = "歡迎修課")
+                Text(text = "歡迎", color = Color.Blue)
+                Text(text = "修課", color = Color.Red)
+                Image(
+                    painterResource(id = R.drawable.teacher),
+                    contentDescription ="teacher icon")
             }
 
             Button(
                 onClick = {
-                   // Toast.makeText(context, "展翅飛翔", Toast.LENGTH_SHORT).show()
-                }
+                    mper?.release()  //釋放資源
+                    mper = null // 清除舊引用
+                    mper = MediaPlayer.create(context, R.raw.fly) //設定音樂
+                    mper?.start()  },  //開始播放
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .fillMaxHeight(0.4f),
+                colors = buttonColors(Color.Blue)
             ) {
-                Text(text = "展翅飛翔")
+                Text(text = "展翅飛翔", color = Color.White)
+                Image(
+                    painterResource(id = R.drawable.fly),
+                    contentDescription ="fly icon")
             }
 
             Button(
                 onClick = {
                     // 結束 App
-                    // (context as? Activity)?.finish()
-                }
+                    val activity = context as? Activity
+                    activity?.finish()
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00BFFF)),
+                shape = CutCornerShape(10),
+                border = BorderStroke(1.dp, Color.Blue),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp)
             ) {
                 Text(text = "結束App")
             }
